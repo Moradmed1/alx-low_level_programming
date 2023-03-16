2296ce25bfc4d0b06e67c6a19bcae1b5e2b2254c
moradmed1 <moradmed1@gmail.com>
2023-03-16 08:05:54 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
-* main - A program that prints a line to the stand error
-* Return: 1 (success)
-*/
-int main(void)
-{
-	fput(\r", stdout);
-return(1);
-}
+	#include <unistd.h>
+	/**
+ 	* main - Entry point
+ 	* Return: 0 (Succes)
+ 	*/
+	int main(void) {
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+	}
