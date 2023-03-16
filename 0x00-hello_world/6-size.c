bac56fc1368972b1b3758fc237a2ad1a9bc4da6c
moradmed1 <moradmed1@gmail.com>
2023-03-16 06:23:52 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - A program that print the size of various camputer types
+* Return: 0 (success)
+*/
+int main(void)
+        char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	
+printf("size of a char: %lu byte(s)\n, (unsigned long)siseof(a));
+printf("size of a int:% lu byte(s)\n, (unsigned long)siseof(b));
+printf("size of a long int: %lu byte(s)\n, (unsigned long)siseof(c));
+printf("size of a long long int: %lu byte(s)\n, (unsigned long)siseof(d));
+printf("size of a float: %lu byte(s)\n, (unsigned long)siseof(f));
+return (0);
+}
