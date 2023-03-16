e80fa5d5efb1bcabcc3996551cf16004418fbd9a
moradmed1 <moradmed1@gmail.com>
2023-03-16 08:04:19 -0700

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - A program that prints a line to the stand error
+* Return: 1 (success)
+*/
+int main(void)
+{
+	fput(\r", stdout);
+return(1);
+}
