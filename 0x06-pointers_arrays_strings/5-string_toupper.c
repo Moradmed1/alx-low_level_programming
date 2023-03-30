00f79c69d001afd2b7e579a92091ceaaf3670356
moradmed1 <moradmed1@gmail.com>
2023-03-30 03:10:03 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	for (i = 0 ; n[i] != '\0' ; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
