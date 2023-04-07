12a870be080e01d74abb07275ea770be2943a3db
moradmed1 <moradmed1@gmail.com>
2023-04-07 02:29:21 -0700

    f

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints the name of the  program
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 if true, 1 if false
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int tot = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			tot = tot + atoi(argv[i]);
+		}
+		printf("%d\n", tot);
+	}
+	return (0);
+}
