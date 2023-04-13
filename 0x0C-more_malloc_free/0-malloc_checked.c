1f07bc708a284d936fd908bd83fc5f659c2bf8ea
moradmed1 <moradmed1@gmail.com>
2023-04-13 08:49:18 -0700

    P

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * *malloc_checked - allocates memory using malloc and exit if failed
  * @b: int
@@ -8,11 +9,11 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *i;
-	i = malloc(b);
+	void *i = malloc(b);
 
-	if (i == 0)
+	if (i == NULL)
+	{
 		exit(98);
-
+	}
 	return (i);
 }
