485e18668d62cd72f25a06649738e9d9ce2b78e0
moradmed1 <moradmed1@gmail.com>
2023-04-17 08:02:32 -0700

    a

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifdef _FUNCTION_LIKE_MACRO_H
+#ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABC(x)((x) < (0) ? -(x) : (x))
+#define ABs(x)((x) < (0) ? -(x) : (x))
 
 #endif
