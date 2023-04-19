7ad03d6e45f3a5fd9c7a8bf30997295e15154c07
moradmed1 <moradmed1@gmail.com>
2023-04-19 13:37:43 -0700

    s

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * main - print opcod
  * @argc: number args
@@ -9,10 +8,10 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *p = (char *)main;
-	int b;
+	int b, j;
+	char *arr;
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
@@ -20,7 +19,15 @@ int main(int argc, char **argv)
 	if (b < 0)
 		printf("Error\n"), exit(2);
 
-	while (b--)
-		printf("%2hhx%s", *p++, b ? " " : "\n");
+	arr = (char *)main;
+	for (j = 0; j < b; j++)
+	{
+		if (j == b - 1)
+		{
+			printf("%02hhx\n", arr[j]);
+			break;
+		}
+		printf("%02hhx ", arr[j]);
+	}
 	return (0);
 }
