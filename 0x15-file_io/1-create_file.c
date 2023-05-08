908f4671da0cc0e976c26736d979f389ad865ab0
Moradmed1 <Moradmed1@gmail.com>
2023-05-08 03:59:41 -0700

    d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 	for (nbr = 0; text_content[nbr]; nbr++)
+		;
 	rw = write(fd, text_content, nbr);
 
 	if (rw == -1)
