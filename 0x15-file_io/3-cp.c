67202fe09dea699dc9c8c7b909e3ccbbcab7c82b
Moradmed1 <Moradmed1@gmail.com>
2023-05-08 06:15:33 -0700

    d

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char *c_buff(char *file);
-void cl_file(int fd);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * c_buff - allocate 1024
+ * create__buffer - allocate 1024
  * @file: file
  *
  * Return: pointer
  */
-char *c_buff(char *file)
+char *create_buffer(char *file)
 {
 	char *buffer;
 
@@ -20,7 +20,7 @@ char *c_buff(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", file);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -28,10 +28,10 @@ char *c_buff(char *file)
 }
 
 /**
- * cl_file - close
+ * close_file - close
  * @fd: desc
  */
-void cl_file(int fd)
+void close_file(int fd)
 {
 	int m;
 
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer = c_buff(argv[2]);
+	buffer = create_buffer(argv[2]);
 	fr = open(argv[1], O_RDONLY);
 	rw = read(fr, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 		if (fr == -1 || rw == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -89,10 +89,13 @@ int main(int argc, char *argv[])
 
 		rw = read(fr, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
+
 	} while (rw > 0);
 
 	free(buffer);
-	cl_file(to);
+	close_file(fr);
+	close_file(to);
+
 	return (0);
 
 }
