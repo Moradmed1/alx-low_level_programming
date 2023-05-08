c0973816642512bfc30538e644cc83eaa2ec0657
Moradmed1 <Moradmed1@gmail.com>
2023-05-08 13:42:42 -0700

    d

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,33 +6,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ch_elf(unsigned char *p_id);
-void pr_magic(unsigned char *p_id);
-void pt_class(unsigned char *p_id);
-void pt_data(unsigned char *p_id);
-void pt_ver(unsigned char *p_id);
-void pt_abi(unsigned char *p_id);
-void pt_osabi(unsigned char *p_id);
-void pt_type(unsigned int e_type, unsigned char *p_id);
-void pt_entry(unsigned long int e_entry, unsigned char *p_id);
+void ck_elf(unsigned char *e_ident);
+void pt_magic(unsigned char *e_ident);
+void pt_class(unsigned char *e_ident);
+void pt_data(unsigned char *e_ident);
+void pt_version(unsigned char *e_ident);
+void pt_abi(unsigned char *e_ident);
+void pt_osabi(unsigned char *e_ident);
+void pt_type(unsigned int e_type, unsigned char *e_ident);
+void pt_entry(unsigned long int e_entry, unsigned char *e_ident);
 void clo_elf(int elf);
 
 /**
- * ch_elf – does Checks if a file is an ELF file.
- * @p_id: is A pointer to magic numbers.
+ * ck_elf - Checks if a file is an ELF file.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
- * Description: If not an ELF file - exit code 98.
+ * Description: If the file is not an ELF file - exit code 98.
  */
-void ch_elf(unsigned char *p_id)
+void ck_elf(unsigned char *e_ident)
 {
-	int idx;
+	int index;
 
-	for (idx = 0; idx < 4; idx++)
+	for (index = 0; index < 4; index++)
 	{
-		if (p_id[idx] != 127 &&
-		    p_id[idx] != 'E' &&
-		    p_id[idx] != 'L' &&
-		    p_id[idx] != 'F')
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
@@ -41,22 +41,22 @@ void ch_elf(unsigned char *p_id)
 }
 
 /**
- * pr_magic – deos Prints the magic nbrs of an ELF header.
- * @p_id: is A pointer to magic numbers.
+ * pt_magic - Prints the magic numbers of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
- * Description: Magic nbr separated by spaces.
+ * Description: Magic numbers are separated by spaces.
  */
-void pr_magic(unsigned char *p_id)
+void pt_magic(unsigned char *e_ident)
 {
-	int idx;
+	int index;
 
 	printf(" Magic: ");
 
-	for (idx = 0; idx < EI_NIDENT; idx++)
+	for (index = 0; index < EI_NIDENT; index++)
 	{
-		printf("%02x", p_id[idx]);
+		printf("%02x", e_ident[index]);
 
-		if (idx == EI_NIDENT - 1)
+		if (index == EI_NIDENT - 1)
 			printf("\n");
 		else
 			printf(" ");
@@ -64,14 +64,14 @@ void pr_magic(unsigned char *p_id)
 }
 
 /**
- * pt_class – does Prints class of an ELF hr.
- * @p_id: is A pointer to ELF class.
+ * pt_class - Prints the class of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void pt_class(unsigned char *p_id)
+void pt_class(unsigned char *e_ident)
 {
 	printf(" Class: ");
 
-	switch (p_id[EI_CLASS])
+	switch (e_ident[EI_CLASS])
 	{
 	case ELFCLASSNONE:
 		printf("none\n");
@@ -83,19 +83,19 @@ void pt_class(unsigned char *p_id)
 		printf("ELF64\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", p_id[EI_CLASS]);
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
 /**
- * pt_data – does Prints   data ELF header.
- * @p_id: is A pointer to an array ELF class.
+ * pt_data - Prints the data of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void pt_data(unsigned char *p_id)
+void pt_data(unsigned char *e_ident)
 {
 	printf(" Data: ");
 
-	switch (p_id[EI_DATA])
+	switch (e_ident[EI_DATA])
 	{
 	case ELFDATANONE:
 		printf("none\n");
@@ -107,20 +107,20 @@ void pt_data(unsigned char *p_id)
 		printf("2's complement, big endian\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", p_id[EI_CLASS]);
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
 /**
- *  * pt_ver – does Prints the ver of ELF header.
- *   * @p_id: is pointer to an array ELF ver.
+ *  * pt_version - Prints the version of an ELF header.
+ *   * @e_ident: A pointer to an array containing the ELF version.
  *    */
-void pt_ver(unsigned char *p_id)
+void pt_version(unsigned char *e_ident)
 {
 	 printf(" Version: %d",
-			  p_id[EI_VERSION]);
+			  e_ident[EI_VERSION]);
 
-	switch (p_id[EI_VERSION])
+	switch (e_ident[EI_VERSION])
 	{
 	case EV_CURRENT:
 		printf(" (current)\n");
@@ -132,14 +132,14 @@ void pt_ver(unsigned char *p_id)
 }
 
 /**
- * pt_osabi – does Prints the OS/ABI of ELF header.
- * @p_id: is pointer to an array ELF version.
+ * pt_osabi - Prints the OS/ABI of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
  */
-void pt_osabi(unsigned char *p_id)
+void pt_osabi(unsigned char *e_ident)
 {
 	printf(" OS/ABI: ");
 
-	switch (p_id[EI_OSABI])
+	switch (e_ident[EI_OSABI])
 	{
 	case ELFOSABI_NONE:
 		printf("UNIX - System V\n");
@@ -172,28 +172,28 @@ void pt_osabi(unsigned char *p_id)
 		printf("Standalone App\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", p_id[EI_OSABI]);
+		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
 }
 
 /**
- * pt_abi – does Prints the ABI ver elf.
- * @p_id: is pointer to an array ELF ABI version.
+ * pt_abi - Prints the ABI version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF ABI version.
  */
-void pt_abi(unsigned char *p_id)
+void pt_abi(unsigned char *e_ident)
 {
 	printf(" ABI Version: %d\n",
-		p_id[EI_ABIVERSION]);
+		e_ident[EI_ABIVERSION]);
 }
 
 /**
- * pt_type – does Prints the type of an ELF header.
+ * pt_type - Prints the type of an ELF header.
  * @e_type: The ELF type.
- * @p_id: is pointer to an array  ELF class.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void pt_type(unsigned int e_type, unsigned char *p_id)
+void pt_type(unsigned int e_type, unsigned char *e_ident)
 {
-	if (p_id[EI_DATA] == ELFDATA2MSB)
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
 	printf(" Type: ");
@@ -221,22 +221,22 @@ void pt_type(unsigned int e_type, unsigned char *p_id)
 }
 
 /**
- * pt_entry – does Prints  entry point of ELF head.
- * @e_entry: is address of ELF entry point.
- * @p_id: is pointer to an array ELF class.
+ * pt_entry - Prints the entry point of an ELF header.
+ * @e_entry: The address of the ELF entry point.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void pt_entry(unsigned long int e_entry, unsigned char *p_id)
+void pt_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 	printf(" Entry point address: ");
 
-	if (p_id[EI_DATA] == ELFDATA2MSB)
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
 			  ((e_entry >> 8) & 0xFF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 
-	if (p_id[EI_CLASS] == ELFCLASS32)
+	if (e_ident[EI_CLASS] == ELFCLASS32)
 		printf("%#x\n", (unsigned int)e_entry);
 
 	else
@@ -247,7 +247,7 @@ void pt_entry(unsigned long int e_entry, unsigned char *p_id)
  * clo_elf - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
  *
- * Description: If cant be closed - exit code 98.
+ * Description: If the file cannot be closed - exit code 98.
  */
 void clo_elf(int elf)
 {
@@ -260,14 +260,14 @@ void clo_elf(int elf)
 }
 
 /**
- * main – does Displays the info contained in the
+ * main - Displays the information contained in the
  * ELF header at the start of an ELF file.
- * @argc: is nbr of arg supplied to the program.
- * @argv: An array of pointers to the args.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0.
+ * Return: 0 on success.
  *
- * Description: If is not an ELF File or
+ * Description: If the file is not an ELF File or
  * the function fails - exit code 98.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
@@ -297,16 +297,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	ch_elf(header->p_id);
+	ck_elf(header->e_ident);
 	printf("ELF Header:\n");
-	pr_magic(header->p_id);
-	pt_class(header->p_id);
-	pt_data(header->p_id);
-	pt_ver(header->p_id);
-	pt_osabi(header->p_id);
-	pt_abi(header->p_id);
-	pt_type(header->e_type, header->p_id);
-	pt_entry(header->e_entry, header->p_id);
+	pt_magic(header->e_ident);
+	pt_class(header->e_ident);
+	pt_data(header->e_ident);
+	pt_version(header->e_ident);
+	pt_osabi(header->e_ident);
+	pt_abi(header->e_ident);
+	pt_type(header->e_type, header->e_ident);
+	pt_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	clo_elf(o);
