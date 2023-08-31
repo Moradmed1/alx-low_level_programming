0a78e0053af09924f49f369804e3a1d5d7d2d19b
Moradmed1 <Moradmed1@gmail.com>
2023-08-31 08:14:19 -0700

    My first commit1

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - returns nbre bit
+ * @n: nbre one
+ * @m: nbre two
+ *
+ * Return: nbre bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbre;
+
+	for (nbre = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbre++;
+	}
+	return (nbre);
+}
