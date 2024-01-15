fc844ddea1068b9efe7d032b0af68e3c5f6d0b4b
Moradmed1 <Moradmed1@gmail.com>
2024-01-15 06:25:08 -0800

    d

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+	if (m == 0)
+		return (NULL);
+		else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+4-frekk_gridc
